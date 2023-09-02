dff74aa547181f75c6674bc674baea4385f7ae32
Upendra-120 <131613521+Upendra-120@users.noreply.github.com>
2023-09-02 11:17:18 +0530

    Add files via upload

---
+++ Untitled6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a=12.5,b=4;
+	int addition,subtraction,multiplication,division,modulus;
+	addition=a+b;
+	subtraction=a-b;
+	multiplication=a*b;
+	division=a/b;
+	modulus=a%b;
+	printf("\n%d",addition);
+	printf("\n%d",subtraction);
+	printf("\n%d",multiplication);
+	printf("\n%d",modulus);
+	printf("\n%d",division);
+	
+	
+}
